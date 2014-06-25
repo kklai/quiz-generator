b8eaadd7739b5fafdb375143f0d3dd91e17bdcc4
kklai <kkrebeccalai@gmail.com>
2014-06-25 16:29:53 -0400

    heroku stuff

--- config.rb
+++ config.rb
@@ -59,6 +59,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :directory_indexes
+set :build_dir, "tmp"
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
