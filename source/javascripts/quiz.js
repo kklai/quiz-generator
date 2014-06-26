5821a925f8ee7c4a3abf133682bd7ec700b6675f
kklai <kkrebeccalai@gmail.com>
2014-06-26 12:23:37 -0400

    loading stylesheets

--- source/javascripts/quiz.js
+++ source/javascripts/quiz.js
@@ -1,6 +1,7 @@
 var ans;
 var score = 0;
 var currentQuestion = 0;
+var pub_stylesheet;
 
 // social media
 var facebook = "<a href='https://www.facebook.com/Vox' target='_blank'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' style='height: 2em;'><circle cx='8' cy='8' r='8' class='shape-1'></circle><path fill='#fff' d='M8.5 3.7h1.4v1.6h-1c-.2 0-.4.1-.4.4v.9h1.4l-.1 1.7h-1.3v4.5h-1.9v-4.5h-.9v-1.7h.9v-1c0-.7.4-1.9 1.9-1.9z' class='shape-2'></path></svg></a>";
@@ -8,7 +9,6 @@ var twitter = "<a href='https://twitter.com/voxdotcom' target='_blank'><svg xmln
 var google = "<a href='https://plus.google.com/110525221945179942553' rel='publisher' target='_blank'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' style='height: 2em;'><circle cx='8' cy='8' r='8' class='shape-1'></circle><path fill='#fff' d='M8.6 4.3l.6-.4c.1-.1.1-.1.1-.2s-.1-.1-.2-.1h-2.7c-.3 0-.6.1-.9.2-1 .3-1.6 1.1-1.6 2 0 1.2.9 2.1 2.2 2.1-.1 0-.1.1-.1.2 0 .2 0 .4.1.5-1.1 0-2.2.6-2.6 1.4-.1.2-.2.4-.2.7 0 .2.1.4.2.6.3.5.8.8 1.5 1 .4.1.8.1 1.2.1.4 0 .7 0 1.1-.1 1-.3 1.7-1.1 1.7-2 0-.8-.2-1.3-1-1.8-.3-.2-.6-.6-.6-.7 0-.2 0-.3.4-.6.5-.4.8-1 .8-1.5s-.2-1-.4-1.3h.2c.1 0 .1 0 .2-.1zm-3.3 1.3c-.1-.4 0-.8.3-1.1.1-.2.3-.2.5-.2.6 0 1.1.7 1.2 1.4.1.4 0 .8-.3 1.1-.1.2-.3.3-.5.3-.6 0-1.1-.7-1.2-1.5zm2.6 4.6v.2c0 .8-.6 1.2-1.7 1.2-.9 0-1.5-.5-1.5-1.2 0-.6.8-1.2 1.7-1.2.2 0 .4 0 .6.1l.2.1c.4.4.7.5.7.8z' class='shape-2'></path><path fill='#fff' d='M13.3 7.8c0 .1-.1.2-.2.2h-1.5v1.5c0 .1-.1.2-.2.2h-.4c-.1 0-.2-.1-.2-.2v-1.5h-1.6c-.1 0-.2-.1-.2-.2v-.4c0-.1.1-.2.2-.2h1.5v-1.5c0-.1.1-.2.2-.2h.4c.1 0 .2.1.2.2v1.5h1.5c.1 0 .2.1.2.2v.4z' class='shape-3'></path></svg></a>";
 
 function buildQuiz() {
-	addcss();
 	qnumber = currentQuestion + 1;
 	$(".quiz-container").html("<div class='qhead'><div class='number'>" + qnumber + "</div><p class='score'>Score: " + score + "/" + input.length + "</p></div><div class='question'>" + input[currentQuestion].question + "</div>" +
 		"<ol class='answers'><li id='option-a'>" + input[currentQuestion].a + "</li>" +
@@ -76,8 +76,11 @@ function finalScore() {
 	$(".quiz-container").html("<div class='scorecard'><p>You correctly answered</p><p>" + score + "/" + input.length + "</p><div id='social-media'><ul><li>" + facebook + "</li><li>" + twitter + "</li><li>" + google + "</li></ul></div><p>Challenge your friends!</p></div>");
 }
 
-function addcss() {
+function quiz() {
 	$('head').append('<link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/quiz-generator/quiz.css" type="text/css" />');
 	$('head').append('<link rel="stylesheet" href="' + pub_stylesheet + '" type="text/css" />');
-	
 }
+
+$(window).load(function(){
+	buildQuiz();
+})
